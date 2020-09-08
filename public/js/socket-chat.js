20768f39abd2f98af864e0427423cbb653712207
Andrés Vargas <andvargas25@gmail.com>
2020-09-08 18:15:55 -0300

    Se termina el chat

--- public/js/socket-chat.js
+++ public/js/socket-chat.js
@@ -4,18 +4,24 @@ var params = new URLSearchParams(window.location.search);
 
 if (!params.has('nombre') || !params.has('sala')) {
     window.location = 'index.html';
-    throw new Error('El nombre y la sala son necesario');
+    throw new Error('El nombre y sala son necesarios');
 }
 
 var usuario = {
     nombre: params.get('nombre'),
     sala: params.get('sala')
-}
+};
+
+
+
 socket.on('connect', function() {
     console.log('Conectado al servidor');
+
     socket.emit('entrarChat', usuario, function(resp) {
-        console.log('Los usuarios conectados', resp);
+        //console.log('Usuarios conectados', resp);
+        renderizarUsuarios(resp);
     });
+
 });
 
 // escuchar
@@ -27,29 +33,29 @@ socket.on('disconnect', function() {
 
 
 // Enviar información
-/*
-socket.emit('crearMensaje', {
-    usuario: 'Fernando',
-    mensaje: 'Hola Mundo'
-}, function(resp) {
-    console.log('respuesta server: ', resp);
-});*/
+// socket.emit('crearMensaje', {
+//     nombre: 'Fernando',
+//     mensaje: 'Hola Mundo'
+// }, function(resp) {
+//     console.log('respuesta server: ', resp);
+// });
 
 // Escuchar información
 socket.on('crearMensaje', function(mensaje) {
-
-    console.log('Servidor:', mensaje);
-
+    renderizarMensaje(mensaje, false);
+    scrollBottom();
 });
 
-//escuchar cambios de usuarios cuando un usuarooentre o sale del chat
-
+// Escuchar cambios de usuarios
+// cuando un usuario entra o sale del chat
 socket.on('listaPersonas', function(personas) {
-    console.log('Servidor:', personas);
-
+    renderizarUsuarios(personas);
+    scrollBottom();
 });
 
-//mensajes privados
+// Mensajes privados
 socket.on('mensajePrivado', function(mensaje) {
-    console.log('mensaje privado', mensaje);
+
+    console.log('Mensaje Privado:', mensaje);
+
 });
